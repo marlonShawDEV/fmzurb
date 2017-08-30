b1c25d44d1512d96124623af2068fa1a562b2965
S Carrero <sherry_carrero@freddiemac.com>
2017-08-30 09:41:42 -0400

    MF nav and page placeholders (#42)
    * basics set up for MF branch development
    
    * updated nav, added page placeholders
    
    * more nav changes, new pages

--- src/assets/js/fakeNav_mf.js
+++ src/assets/js/fakeNav_mf.js
@@ -4,40 +4,58 @@
 function fakeNav(){
   // this will be automated on the backend, faking it for now to show different highlighting/subnavs
   var p = location.pathname;
-  if (p.match(/blog/)) { 
-    $('#nav-blog').children('a').addClass('active');
-    $('#subnav-blog').addClass('on').find('.no-bullet').removeClass('hide');
-    if (p.match(/detail|homeownership/)) {$('#subnav-blog-homeownership').addClass('active');}
-    else if (p.match(/rental/)) {$('#subnav-blog-rental').addClass('active');}
-    else if (p.match(/research/)) {$('#subnav-blog-research').addClass('active');}
-    else if (p.match(/notable/)) {$('#subnav-blog-notable').addClass('active');}
-    else if (p.match(/archive|harp/)) {$('#subnav-blog-archive').addClass('active');}
+  if (p.match(/seller_servicer/)) { 
+    $('#nav-seller-servicers').children('a').addClass('active');
+    $('#subnav-seller-servicers').addClass('on').find('.no-bullet').removeClass('hide');
+    if (p.match(/uw/)) {$('#subnav-seller-servicers-uw').addClass('active');}
+    else if (p.match(/purchase/)) {$('#subnav-seller-servicers-purchase').addClass('active');}
+    else if (p.match(/reporting/)) {$('#subnav-seller-servicers-reporting').addClass('active');}
+    else if (p.match(/asset/)) {$('#subnav-seller-servicers-asset').addClass('active');}
+    else if (p.match(/guide/)) {$('#subnav-seller-servicers-guide').addClass('active');}
+    else if (p.match(/legal/)) {$('#subnav-seller-servicers-legal').addClass('active');}
+  }
+  else if (p.match(/investors/)) { 
+    $('#nav-investors').children('a').addClass('active');
+    $('#subnav-investors').addClass('on').find('.no-bullet').removeClass('hide');
+    if (p.match(/calendars/)) {$('#subnav-investors-calendars').addClass('active');}
+    else if (p.match(/presentations/)) {$('#subnav-investors-presentations').addClass('active');}
+    else if (p.match(/data/)) {$('#subnav-investors-performance').addClass('active');}
+    else if (p.match(/lookup/)) {$('#subnav-investors-lookup').addClass('active');}
+    else if (p.match(/investments/)) {$('#subnav-investors-investments').addClass('active');}
+  }
+  else if (p.match(/borrowers|lenders/)) {
+    $('#nav-borrowers').children('a').addClass('active');
+    $('#subnav-borrowers').addClass('on').find('.no-bullet').removeClass('hide');
+    if (p.match(/find/)) {$('#subnav-borrowers-conventional').addClass('active');}   
+    else if (p.match(/tah/)) {$('#subnav-borrowers-tah').addClass('active');}   
+    else if (p.match(/seniors/)) {$('#subnav-borrowers-seniors').addClass('active');}   
+    else if (p.match(/sbl/)) {$('#subnav-borrowers-sbl').addClass('active');} 
+  }
+  else if (p.match(/product/)) {
+    $('#nav-products').children('a').addClass('active');
+    $('#subnav-products').addClass('on').find('.no-bullet').removeClass('hide');
+    if (p.match(/conventional/)) {$('#subnav-products-conventional').addClass('active');}   
+    else if (p.match(/sbl/)) {$('#subnav-products-sbl').addClass('active');}   
+    else if (p.match(/tah/)) {$('#subnav-products-tah').addClass('active');}   
+    else if (p.match(/seniors/)) {$('#subnav-products-seniors').addClass('active');}   
+    else if (p.match(/lock/)) {$('#subnav-products-lock').addClass('active');}   
+    else if (p.match(/green/)) {$('#subnav-products-green').addClass('active');}   
+  }
+  else if (p.match(/news|aimi|events|blog/)) {
+    $('#nav-news').children('a').addClass('active');
+    $('#subnav-news').addClass('on').find('.no-bullet').removeClass('hide');
+    if(p.match(/blog/)) {$('#subnav-news-blog').addClass('active');}  
+    else if(p.match(/events/)) {$('#subnav-news-events').addClass('active');}  
+    else if(p.match(/aimi/)) {$('#subnav-news-aimi').addClass('active');}  
+    else if(p.match(/research\.html/)) {$('#subnav-news-research').addClass('active');}  
+    else if(p.match(/\/news\//))  {$('#subnav-news-customer').addClass('active');}  
   }
   else if (p.match(/about/)) {
     $('#nav-about').children('a').addClass('active');
     $('#subnav-about').addClass('on').find('.no-bullet').removeClass('hide');
-    if (p.match(/leaders/)) {$('#subnav-about-leaders').addClass('active');}
-    if (p.match(/business/)) {$('#subnav-about-business').addClass('active');}
-    if (p.match(/people|employee|supplier/)) {$('#subnav-about-people').addClass('active');}
-    if (p.match(/communities/)) {$('#subnav-about-communities').addClass('active');}
-    else if (p.match(/governance|annual|agenda|board/)) {$('#subnav-about-governance').addClass('active');}
-    else if (p.match(/investor/)) {$('#subnav-about-investors').addClass('active');}
-  }
-  else if (p.match(/media-/)) {
-    $('#nav-mediaroom').children('a').addClass('active');
-    $('#subnav-mediaroom').addClass('on').find('.no-bullet').removeClass('hide');
-    if (p.match(/detail|archive/)) {$('#subnav-mediaroom-archive').addClass('active');}   
-    else if (p.match(/press/)) {$('#subnav-mediaroom-resources').addClass('active');} 
-  }
-  else if (p.match(/research/)) {
-    $('#nav-research').children('a').addClass('active');
-    $('#subnav-research').addClass('on').find('.no-bullet').removeClass('hide');
-    if(p.match(/pmms/)) {$('#subnav-research-mortgage').addClass('active');} 
-    else if (p.match(/detail|insight/)) {$('#subnav-research-insight').addClass('active');} 
-    else if (p.match(/outlook/)) {$('#subnav-research-outlook').addClass('active');} 
-    else if (p.match(/consumer/)) {$('#subnav-research-consumer').addClass('active');} 
-    else if (p.match(/aimi/)) {$('#subnav-research-indices').addClass('active');}   
-    else if (p.match(/data/)) {$('#subnav-research-datasets').addClass('active');}   
+    if (p.match(/who/)) {$('#subnav-about-who').addClass('active');}
+    if (p.match(/what/)) {$('#subnav-about-what').addClass('active');}
+    if (p.match(/results/)) {$('#subnav-about-results').addClass('active');}
   }
 }
 
